003fe537727dffdcffdcddbcd88839eab87f90f9
RasheedHendawi <rhendawi@apple.com>
2025-08-14 01:48:59 +0300

    chaning index

--- codespace/frontend/src/index.js
+++ codespace/frontend/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 // import './styles/index.css';
 import App from './App';
 
-import './setup';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
