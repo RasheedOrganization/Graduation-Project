c3d7b36cf1ae8427b9d6bb006ee24b0b8dc59337
Mohammad Aburihan <65272881+MohammadAburihan@users.noreply.github.com>
2025-08-14 19:02:04 +0300

    Replace login with profile dropdown after authentication

---
+++ codespace/frontend/src/pages/ProfilePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+
+function ProfilePage() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Profile Page</h1>
+    </div>
+  );
+}
+
+export default ProfilePage;
