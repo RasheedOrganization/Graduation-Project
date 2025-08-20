6a2f37ca979aed7b2269870206a0337ad3c27d54
Mohammad Aburihan <65272881+MohammadAburihan@users.noreply.github.com>
2025-08-20 15:32:48 +0300

    chore: install python deps in setup script

--- codespace/setup.sh
+++ codespace/setup.sh
@@ -1,2 +1,11 @@
+#!/bin/sh
+set -e
+
 docker pull nubskr/compiler:1
-docker network create --internal mynetwork
+# Create internal network if it doesn't already exist
+if ! docker network ls --format '{{.Name}}' | grep -q '^mynetwork$'; then
+  docker network create --internal mynetwork
+fi
+
+# Install Python dependencies required for scraping Codeforces problems
+pip3 install -r "$(dirname "$0")/server/requirements.txt"
