3e3224e6f573b600cf8e195059e4242156c711f4
Mohammad Aburihan <65272881+MohammadAburihan@users.noreply.github.com>
2025-09-10 21:29:57 +0300

    feat(ios): show website in app

--- IOSApp/__tests__/App.test.tsx
+++ IOSApp/__tests__/App.test.tsx
@@ -6,16 +6,13 @@ import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
-jest.mock('@react-native-async-storage/async-storage', () =>
-  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
-);
-
-jest.useFakeTimers();
+jest.mock('react-native-webview', () => {
+  const {View} = require('react-native');
+  return {WebView: View};
+});
 
 const App = require('../App').default;
 
 it('renders correctly', () => {
   renderer.create(<App />);
 });
-
