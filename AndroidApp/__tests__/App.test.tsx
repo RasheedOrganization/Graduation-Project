fb4695d5f5426eed940d92034af96993dab21fb0
Mohammad Aburihan <65272881+MohammadAburihan@users.noreply.github.com>
2025-09-10 19:19:39 +0300

    Remove logo assets

--- AndroidApp/__tests__/App.test.tsx
+++ AndroidApp/__tests__/App.test.tsx
@@ -4,14 +4,18 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
-
-// Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
-
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+);
+
+jest.useFakeTimers();
+
+const App = require('../App').default;
 
 it('renders correctly', () => {
   renderer.create(<App />);
 });
+
--- IOSApp/jest.config.js
+++ IOSApp/jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   preset: 'react-native',
+  moduleNameMapper: {
+    '\\.(png|jpg|jpeg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
 };
